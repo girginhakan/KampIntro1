fc0b2137a0e1770f31bc8188025057723abcd644
hakan girgin <hakangirgin7@gmail.com>
2021-01-20 20:51:38 +0000

    20.01.2020 List

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string> { "Hakan", "Merve", "Gökhan", "Hasan", "Burak" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[4]);
+
+            isimler2.Add("Arif");
+            Console.WriteLine(isimler2[5]);
+
+
+
+        }
+    }
+}
